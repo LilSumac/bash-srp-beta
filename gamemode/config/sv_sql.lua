e559c8a64887b9c8234463700898c56ddeb54adb
LilSumac <lilsumac@gmail.com>
2018-04-02 15:51:21 -0700

    Removed old connection information.

--- gamemode/config/sv_sql.lua
+++ gamemode/config/sv_sql.lua
@@ -1,8 +1,8 @@
 local BASH = BASH;
 BASH.SQL = BASH.SQL or {};
 
-BASH.SQL.Hostname = "45.55.218.30";
-BASH.SQL.Username = "tester";
-BASH.SQL.Password = "testpass";
-BASH.SQL.Database = "srp_db";
+BASH.SQL.Hostname = "localhost";
+BASH.SQL.Username = "username";
+BASH.SQL.Password = "password";
+BASH.SQL.Database = "bash_db";
 BASH.SQL.Port = 3306;
